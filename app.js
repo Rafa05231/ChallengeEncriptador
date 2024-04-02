0a881774129aa5a8d45fc210ffdfc74ad149fa30
Rafael Campo <campo0135@gmail.com>
2024-04-01 19:33:19 -0500

    Se agrego la funcionalidad para desencriptar y copiar

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ function encriptar(){
 
     document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
 }
+
+function desencriptar(){
+    let texto = document.getElementById("textoCifrar").value.toLowerCase();
+
+
+    let textoEncriptado = texto.replace(/enter/img, "e");
+    textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+}
+
+function copiar(){
+    let texto = document.querySelector("#textoEncriptado");
+    texto.select();
+    document.execCommand("copy");
+}
