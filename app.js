f7f5ff722163e88cf1c7a422dea09ea541f40e3b
Rafael Campo <campo0135@gmail.com>
2024-04-01 23:41:37 -0500

    Se agregaron los estilos de la pagina y la funcion para que no se acepten mayusculas, proyecto finalizado

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 function encriptar(){
-    let texto = document.getElementById("textoCifrar").value.toLowerCase();
+    let texto = document.getElementById("textoEncriptado").value;
+    let textoEncriptado = "";
+    let hayMayusculas=false;
 
+    for(let i = 0;i < texto.length;i++){
+        if(texto[i] == texto[i].toUpperCase() && texto[i] != texto[i].toLowerCase()){
+            textoEncriptado = "No se aceptan mayusculas ni carateres especiales";
+            hayMayusculas=true;
+            document.getElementById("texto-encriptado").innerHTML = textoEncriptado;
+            break;
+        }
+    }
 
-    let textoEncriptado = texto.replace(/e/img, "enter");
-    textoEncriptado = textoEncriptado.replace(/i/img, "imes");
-    textoEncriptado = textoEncriptado.replace(/a/img, "ai");
-    textoEncriptado = textoEncriptado.replace(/o/img, "ober");
-    textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+    if(!hayMayusculas){
+            textoEncriptado = texto.replace(/e/mg, "enter");
+            textoEncriptado = textoEncriptado.replace(/i/mg, "imes");
+            textoEncriptado = textoEncriptado.replace(/a/mg, "ai");
+            textoEncriptado = textoEncriptado.replace(/o/mg, "ober");
+            textoEncriptado = textoEncriptado.replace(/u/mg, "ufat");
 
-    document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+            document.getElementById("contenedor-subtitulo").style.display = "none";
+            document.getElementById("texto-encriptado").innerHTML = textoEncriptado;
+            document.getElementById("botonCopiar").style.display = "show";
+            document.getElementById("botonCopiar").style.display = "inherit";
+    }
 }
 
 function desencriptar(){
-    let texto = document.getElementById("textoCifrar").value.toLowerCase();
+    let texto = document.getElementById("textoEncriptado").value;
 
 
-    let textoEncriptado = texto.replace(/enter/img, "e");
-    textoEncriptado = textoEncriptado.replace(/imes/img, "i");
-    textoEncriptado = textoEncriptado.replace(/ai/img, "a");
-    textoEncriptado = textoEncriptado.replace(/ober/img, "o");
-    textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+    let textoEncriptado = texto.replace(/enter/mg, "e");
+    textoEncriptado = textoEncriptado.replace(/imes/mg, "i");
+    textoEncriptado = textoEncriptado.replace(/ai/mg, "a");
+    textoEncriptado = textoEncriptado.replace(/ober/mg, "o");
+    textoEncriptado = textoEncriptado.replace(/ufat/mg, "u");
 
-    document.getElementById("textoEncriptado").innerHTML = textoEncriptado;
+    document.getElementById("contenedor-subtitulo").style.display = "none";
+    document.getElementById("texto-encriptado").innerHTML = textoEncriptado;
+    document.getElementById("botonCopiar").style.display = "show";
+    document.getElementById("botonCopiar").style.display = "inherit";
 }
 
 function copiar(){
-    let texto = document.querySelector("#textoEncriptado");
+    let texto = document.querySelector("#texto-encriptado");
     texto.select();
     document.execCommand("copy");
 }
